e865be9ccf466d6fc0e3ef90f1bfec4cd9103cce
hamdansh <168594848+hamdansh@users.noreply.github.com>
2024-07-14 00:18:14 -0700

    Add files via upload

---
+++ bmi calculator/main.js
@@ -0,0 +1,4 @@
+var weightinkg = 75;
+var heightinmeters = 6;
+var bmi = weightinkg / (heightinmeters * heightinmeters);
+console.log("your bmi is ".concat(bmi));
---
+++ bmi calculator/main.ts
@@ -0,0 +1,4 @@
+let weightinkg = 75;
+let heightinmeters = 6;
+let bmi = weightinkg / (heightinmeters * heightinmeters)
+console.log(`your bmi is ${bmi}`)
